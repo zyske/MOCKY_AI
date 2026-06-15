370680a8ad020dc8e15c534a255b25cb551562f7
Khidhr <121404431+zyske@users.noreply.github.com>
2026-06-16 02:01:09 +0700

    Refactor AgentRegistry to use Agent struct

--- contracts/AgentRegistry.sol
+++ contracts/AgentRegistry.sol
@@ -3,13 +3,30 @@ pragma solidity ^0.8.0;
 
 contract AgentRegistry {
 
-string[] public agents;
-
-function registerAgent(string memory agentName) public {
-    agents.push(agentName);
+struct Agent {
+    string name;
+    string specialization;
 }
 
-function getTotalAgents() public view returns (uint256) {
+Agent[] public agents;
+
+function registerAgent(
+    string memory _name,
+    string memory _specialization
+) public {
+    agents.push(
+        Agent(
+            _name,
+            _specialization
+        )
+    );
+}
+
+function getTotalAgents()
+    public
+    view
+    returns (uint256)
+{
     return agents.length;
 }
 
